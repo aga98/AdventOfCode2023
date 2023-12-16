b4a30641e4087850ca7cd12bf4f05bc2d88e6897
Alberto Gimenez <algimeneza@gmail.com>
2023-12-16 20:03:22 +0100

    Finish Day 13

--- Day 13 - Point of Incidence/13.2.py
+++ Day 13 - Point of Incidence/13.2.py
@@ -1,38 +1,26 @@
 from utils import read_input
-from common import process_input, check_reflections
+from common import process_input
 
 
-def get_reflection_line(valley: list[str], expected_diff: int = 0) -> int | None:
-    for index, row in enumerate(valley[:-1]):
-        row2 = valley[index + 1]
-        matching_chars = [False] * len(row)
-        for i in range(len(row)):
-            matching_chars[i] = row[i] == row2[i]
-        if matching_chars.count(False) == expected_diff:
-            return index
-    return None
+def get_distance(row1: str, row2: str) -> int:
+    return sum(a != b for a, b in zip(row1, row2))
+
+
+def get_reflection_line(block: list[str], distance_to_match: int) -> int:
+    for idx in range(len(block)):
+        if sum(get_distance(l, r) for l, r in zip(reversed(block[:idx]), block[idx:])) == distance_to_match:
+            return idx
 
 
 def find_reflections(valley: list[str]) -> int:
-    result = 0
-    transposed_valley = ["".join(row) for row in list(map(list, zip(*valley)))]
-    horizontal_reflection_line = get_reflection_line(valley, expected_diff=1)
-    vertical_reflection_line = get_reflection_line(transposed_valley, expected_diff=1)
-    if horizontal_reflection_line is not None:
-        result += 100 * (horizontal_reflection_line + 1)
-
-    if vertical_reflection_line is not None:
-        result += vertical_reflection_line + 1
-
-    return result
-
+    if row := get_reflection_line(valley, 1):
+        return 100 * row
+    
+    if col := get_reflection_line(list(zip(*valley)), 1):
+        return col
 
 
 if __name__ == "__main__":
-    valleys = process_input(read_input(load_dummy=True))
-    reflection_number = 0
-    for val_i, val in enumerate(valleys):
-        reflections = find_reflections(val)
-        print(f"Reflection calculation for valley {val_i}: {reflections}")
-        reflection_number += reflections
+    valleys = process_input(read_input(load_dummy=False))
+    reflection_number = sum(find_reflections(block) for block in valleys)
     print(f"Total reflection calculation: {reflection_number}")
