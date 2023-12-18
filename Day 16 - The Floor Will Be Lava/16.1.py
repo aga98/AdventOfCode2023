36a75a9390083cead58cca96df924f65a4ec6892
Alberto Gimenez <algimeneza@gmail.com>
2023-12-18 19:52:04 +0100

    Day 16 Part 1

--- Day 16 - The Floor Will Be Lava/16.1.py
+++ Day 16 - The Floor Will Be Lava/16.1.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from utils import read_input
 
 DIRECTIONS = {
@@ -10,25 +12,6 @@ DIRECTIONS = {
 
 def process_input(lines: list[str]) -> list[list[str]]:
     return [list(line) for line in lines]
-
-
-class Cavern:
-    def __init__(self, cavern: list[list[str]]):
-        self.cavern = cavern
-        self.beams = [Beam(0, 0, "E")]
-        self.visited_tiles = set()
-    
-    def move_beams(self):
-        while len(self.beams) > 0:
-            for beam in self.beams:
-                new_beams = beam.move(self.cavern)
-                if not beam.ended:
-                    self.visited_tiles.add((beam.i, beam.j))
-                if new_beams is not None:
-                    new_beam_1, new_beam_2 = new_beams
-                    self.beams += [new_beam_1, new_beam_2]
-                else:
-                    self.beams.remove(beam)
 
 
 class Beam:
@@ -76,10 +59,47 @@ class Beam:
                 else:
                     self.i = next_i
                     self.j = next_j
+
+
+class Cavern:
+    def __init__(self, cavern: list[list[str]], start_beam: Beam = Beam(0, -1, "E")):
+        self.cavern = cavern
+        self.beams = [start_beam]
+        self.visited_tiles = defaultdict(list)
+        
+    def _add_beam_if_not_visited(self, beam: Beam):
+        visited_directions = self.visited_tiles.get((beam.i, beam.j), [])
+        if beam.direction not in visited_directions:
+            self.visited_tiles[(beam.i, beam.j)].append(beam.direction)
+            self.beams.append(beam)
+    
+    def move_beams(self):
+        while self.beams:
+            beam = self.beams.pop()
+            new_beams = beam.move(self.cavern)
+            if beam.ended:
+                continue
+            elif not beam.ended and new_beams is None:
+                self._add_beam_if_not_visited(beam)
+            elif new_beams is not None:
+                new_beam_1, new_beam_2 = new_beams
+                self._add_beam_if_not_visited(new_beam_1)
+                self._add_beam_if_not_visited(new_beam_2)
+            # self.print_visited_tiles()
                 
+    def print_visited_tiles(self):
+        for i in range(len(self.cavern)):
+            for j in range(len(self.cavern[0])):
+                if (i, j) in self.visited_tiles:
+                    print("# ", end="")
+                else:
+                    print(self.cavern[i][j], end=" ")
+            print()
+        print()
+
 
 def find_tiles_energized():
-    c = process_input(read_input(load_dummy=True))
+    c = process_input(read_input(load_dummy=False))
     cavern = Cavern(c)
     cavern.move_beams()
     print(f"Part 1 - tiles visited: {len(cavern.visited_tiles)}")
