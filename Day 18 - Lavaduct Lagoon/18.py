60581787552060360543cf0b0bb5da28ad4d57a9
Alberto Gimenez <algimeneza@gmail.com>
2023-12-19 21:51:25 +0100

    Day 18 part 1

--- Day 18 - Lavaduct Lagoon/18.py
+++ Day 18 - Lavaduct Lagoon/18.py
@@ -1,7 +1,4 @@
-from typing import Tuple, List, Set
-
 from utils import read_input
-
 
 DIRECTIONS = {
     "U": (-1, 0),
@@ -33,56 +30,67 @@ def build_matrix(dug: set[tuple[int, int]]):
     min_row, max_row = min(iss), max(iss)
     jss = [j for _, j in dug]
     min_col, max_col = min(jss), max(jss)
-    final_dug_matrix = [["."] * (max_col - min_col + 1) for _ in range(max_row - min_row + 1)]
+    
+    # simplify by adding a padding
+    padding = 1
+    final_dug_matrix = [["."] * (max_col - min_col + 1 + 2 * padding) for _ in range(max_row - min_row + 1 + 2 * padding)]
     for i, j in dug:
-        new_i = i - min_row
-        new_j = j - min_col
+        new_i = i - min_row + 1
+        new_j = j - min_col + 1
         final_dug_matrix[new_i][new_j] = "#"
     return final_dug_matrix
 
     
-def dig_edge(instructions: list[tuple[str, int, str]]) -> list[list[str]]:
+def dig_edge(instructions: list[tuple[str, int, str]]) -> list[tuple[int, int]]:
     pos = (0, 0)
-    dug = {pos}
+    dug = [pos]
     for direction, meters, _ in instructions:
         new_pos = pos
         for i in range(1, meters + 1):
             new_pos = (pos[0] + i * DIRECTIONS[direction][0], pos[1] + i * DIRECTIONS[direction][1])
             if new_pos not in dug:
-                dug.add(new_pos)
+                dug.append(new_pos)
         pos = new_pos
     matrix = build_matrix(dug)
     print_matrix(matrix)
-    return matrix
+    return dug
 
     
-def dig_inside(matrix):
-    original_matrix = matrix.copy()
-    cubes = set()
-    for i in range(len(matrix)):
-        crosses = 0
-        for j in range(len(matrix[0])):
-            previous_elems = matrix[i][j-2:j+1]
-            # check left border
+def calculate_inside_points(loop_points: list[tuple[int, int]]):
+    """
+    Pick's theorem: Suppose that a polygon has integer coordinates for all of its vertices.
+    Let i be the number of integer points interior to the polygon, and
+    let b be the number of integer points on its boundary
+    (including both vertices and points along the sides).
+    Then the area A of this polygon is
+    A = i + b/2 - 1  --> i = A - b/2 + 1
 
-            if j == 0 and original_matrix[i][j] == "#" or original_matrix[i][j] == "#" and original_matrix[i][j - 1] != "#":
-                crosses += 1
-                cubes.add((i, j))
+    We need to calculate the area of the polygon and the number of points on its boundary.
+    The shoelace formula, also known as Gauss's area formula and the surveyor's formula,
+    is a mathematical algorithm to determine the area of a simple polygon
+    whose vertices are described by their Cartesian coordinates in the plane.
+    There are multiple formulas, but we can use the following one:
+    A = 1/2 * sum[i=0 to n-1] [y_i(x_i-1 - x_i+1)]
+    """
+    def calculate_polygon_area(points: list[tuple[int, int]]) -> float:
+        """Shoelace formula A = 1/2 * sum[i=0 to n-1] [y_i(x_i-1 - x_i+1)]"""
+        polygon_area = 0
+        xs = [x for x, _ in points]
+        ys = [y for _, y in points]
+        for i in range(len(points)):
+            polygon_area += (ys[i] * (xs[i - 1] - xs[(i + 1) % len(xs)]))
+        return abs(polygon_area) / 2
 
-            elif j > 1 and original_matrix[i][j-2]:
-                crosses += 1
-
-            if crosses % 2 == 1:  # crossed an odd number of times -> inside
-                matrix[i][j] = "#"
-                cubes.add((i, j))
-
-    print_matrix(matrix)
-    return cubes
+    area = calculate_polygon_area(loop_points)
+    boundary = len(loop_points)
+    inside = int(area - boundary / 2 + 1)
+    print(f"Area: {area}, Boundary: {boundary} --> Inside: {inside}")
+    return inside + boundary
 
 
 if __name__ == "__main__":
     plan = process_input(read_input(load_dummy=False))
-    m = dig_edge(plan)
-    total_cubes = dig_inside(m)
-    print("Total cubes:", len(total_cubes))
+    edge = dig_edge(plan)
+    total_cubes = calculate_inside_points(edge)
+    print("Total cubes:", total_cubes)
 
